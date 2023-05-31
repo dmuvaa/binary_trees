71817b280d59e62d3d2500835ae447e8e64702fa
Dennis Muvaa <dmuvaa70@gmail.com>
2023-05-31 12:59:14 -0700

    Instert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -32,7 +32,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 
 	new_node->left = NULL;
-	parent->left = new_node;
+	parent->right = new_node;
 
 	return (new_node);
 }
