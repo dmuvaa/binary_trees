d53a69d0a0a2fd4de3d9fdf40e3161716aa86ffa
Dennis Muvaa <dennis@DESKTOP-4RKO96U>
2023-05-31 23:21:18 +0300

    Delete Binary Tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes a binary tree
+ * @tree: tree to be deleted
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	/*Check if the tree exists. If not, do nothing */
+	if (!tree)
+		return;
+
+	/* if right child exists, delete the right subtree */
+	if (tree->right)
+		binary_tree_delete(tree->right);
+
+	/* if left child exists, delete the left subtree*/
+	if (tree->left)
+		binary_tree_delete(tree->left);
+
+	/* delete the node after deleting subtrees */
+	free (tree);
+}
