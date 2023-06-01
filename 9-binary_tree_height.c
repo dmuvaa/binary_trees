a1b32e0388adafebbc945c9e197e114ac31dfdff
Dennis Muvaa <dennis@DESKTOP-4RKO96U>
2023-06-01 17:07:32 +0300

    Calculate height of Binary Tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,32 +8,31 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0;
+	
+	size_t right_height = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	if (tree->left == NULL)
+	if (tree->left)
 	{
-		return (0);
+		left_height = binary_tree_height(tree->left) + 1;
 	}
 
-	if (tree->right == NULL)
+	if (tree->right)
 	{
-		return (0);
+		right_height = binary_tree_height(tree->right) +1;
 	}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height >= right_height)
+	if (left_height > right_height)
 	{
-		return (left_height + 1);
+		return (left_height);
 	}
 	else
 	{
-		return (right_height + 1);
+		return (right_height);
 	}
 }
