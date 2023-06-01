93dbffb7a7d58103e85624d659178d2d552417d4
Dennis Muvaa <dennis@DESKTOP-4RKO96U>
2023-06-01 20:02:17 +0300

    function that checks if a binary tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,12 +13,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left == NULL)
-	{
-		return (1);
-	}
-
-	if (tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (1);
 	}
