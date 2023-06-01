d1f4edab207a933c6d18a0cf233805ecf25dd259
Dennis Muvaa <dennis@DESKTOP-4RKO96U>
2023-06-01 16:38:41 +0300

    Function to Get Height of the Tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,29 +8,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	binary_tree_t *right_height;
-	binary_tree_t *left_height;
-
-	right_height = malloc(sizeof(binary_tree_t));
-	left_height = malloc(sizeof(binary_tree_t));
-
-	if (parent == NULL)
+	if (tree == NULL)
 	{
 		return (0);
 	}
 
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	if (left_height >= right_height)
+	{
+		return (left_height + 1);
+	}
 	else
 	{
-		int left_height = binary_tree_height(parent->NULL);
-		int right_height = binary_tree_height(parent->NULL);
-
-		if (left_height >= right_height)
-		{
-			return (left_height + 1);
-		}
-		else
-		{
-			return (right_height + 1);
-		}
+		return (right_height + 1);
 	}
 }
