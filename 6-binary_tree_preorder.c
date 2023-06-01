b18976fd78b4cadea34f93310b0c2c003b6dec2f
Dennis Muvaa <dennis@DESKTOP-4RKO96U>
2023-06-01 13:03:12 +0300

    Preorder traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_preorder- traverses BT in pre-order
+ * @tree: pointer to object
+ * @func: fuction pointer as a prameter
+ * Return: Nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+
+	if (func == NULL)
+	{
+		return;
+	}
+
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+
+	binary_tree_preorder(tree->right, func);
+}
